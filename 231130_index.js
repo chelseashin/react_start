5009f722ff3bef0a12ed0a2a9d7ac2d34b37aa07
chelseashin <chaewonshin95@gmail.com>
2023-12-08 16:47:49 +0900

    231206 | react start

--- 231130_index.js
+++
@@ -1,8 +0,0 @@
-console.log("Hello Node js");
-
-// calc.js에 있는 모듈 가져다 쓰기
-const calc = require("./calc");
-
-console.log(calc.add(12, 80))
-console.log(calc.add(234, 987))
-console.log(calc.sub(987, 123))
