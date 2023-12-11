b3b1d35e7797f4fa2a3b4d17976c1ff075c94dee
chelseashin <chaewonshin95@gmail.com>
2023-12-11 17:24:44 +0900

    231211 | 1 증가/감소 Counter 컴포넌트 만들어 props로 연결하기

--- react_exam1/src/App.js
+++ react_exam1/src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 // import './App.css';
 
 import MyHeader from './MyHeader';
 import MyFooter from './MyFooter';
+import Counter from './Counter';
 
 function App() {
   let name = "채원이";
   const style = {
     App: {
-      backgroundColor: "black"
+      backgroundColor: "white"
     },
     h2: {
       color: "red",
@@ -23,6 +24,14 @@ function App() {
   };
   
   const number = 5;
+  const counterProps = {
+    a: 1,
+    b: 2, 
+    c: 3,
+    d: 4,
+    e: 5,
+    initialValue: 50
+  }
 
   return (
     // <div className="App">
@@ -30,14 +39,16 @@ function App() {
       <MyHeader />
 
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h2 style={style.h2}>안녕 {name}!</h2>
         <b style={style.bold_text} id="bold_text">
           React.js에서는 숫자나 문자열만 표현할 수 있다! {func()} {5+5} <br></br>
           {number}는 {number%2 === 0 ? "짝수" : "홀수"}
         </b>
       </header>
-
+      {/* 부모 컴포넌트인 App컴포넌트에서 자식 컴포넌트인 Counter로 컴포넌트에게 어떤 값을 이름을 붙여서 전달하는 방식을 prop이라고 함 */}
+      {/* <Counter initialValue={10} a={1} b={2} c={3} d={4} e={5} /> */}
+      <Counter {... counterProps}/>  {/* Spread 연산자 사용해 전달하기 */}
       <MyFooter />
     </div>
   );
---
+++ react_exam1/src/Counter.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+// 부모 컴포넌트에서 내려준 props를 매개변수를 통해 받아와 사용
+// const Counter = (props) => {
+const Counter = ({initialValue}) => {    // 비구조화 할당 방식
+    // console.log("Counter 호출!", initialValue);
+    // 0에서 출발, +누르면 1증가, -누르면 1감소, counter 만들기
+    // count: 상태의 값으로 사용, setCount: count라는 값을 변화시키는 상태변화 함수로써 사용 , 0은 초기값으로 사용
+    const [count, setCount] = useState(initialValue);
+    // useState: 배열 반환하고, 배열의 비구조 할당을 통해 0번째 인덱스 카운트, 1번째 인덱스를 setCount라는 상수로 받아온 것을 확인
+
+    const onIncrease = () => {
+        setCount(count + 1);
+    };
+
+    const onDecrease = () => { 
+        setCount(count - 1);
+    };
+
+    return (
+        <div>
+            <h2>{count}</h2>
+            <button onClick={onIncrease}>+</button>
+            <button onClick={onDecrease}>-</button>
+        </div>
+    )
+};
+
+Counter.defaultProps={
+    initialValue: 0
+}
+
+export default Counter;
